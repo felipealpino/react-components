f9f9427d20c75aa6dffe85dbd501c1477ca2262c
felipealpino <felipealpino1@gmail.com>
2022-03-13 17:02:46 -0300

    pagination withKnobs

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,9 +1,20 @@
 import React, { useCallback } from 'react';
-import { storiesOf } from '@storybook/react';
+import { addDecorator, storiesOf } from '@storybook/react';
 import { Pagination } from '../Pagination';
+import { withKnobs, select } from '@storybook/addon-knobs';
+import { boolean } from 'yup';
 
-storiesOf('Pagination', module).add('Default', () => {
-  const callback = useCallback(async () => {}, []);
+storiesOf('Pagination', module)
+  .addDecorator(withKnobs)
 
-  return <Pagination totalOfPages={10} currentPage={0} callbackGetListData={() => callback()} />;
-});
+  .add('Default', () => {
+    const status = select(
+      'Status',
+      ['basic', 'primary', 'secondary', 'tertiary', 'quaternary', 'success', 'info', 'warning', 'danger', 'control'],
+      'primary'
+    );
+
+    const callback = useCallback(async () => {}, []);
+
+    return <Pagination status={status} totalOfPages={10} currentPage={0} callbackGetListData={() => callback()} />;
+  });
