9fab803bf014881d47b256eb19a69fe820909671
felipealpino <felipealpino1@gmail.com>
2022-03-19 16:56:02 -0300

    accordion

--- src/components/Accordion/styles.ts
+++ src/components/Accordion/styles.ts
@@ -1,6 +1,48 @@
 import { IAccordion } from '@components/Accordion';
+import { colorGet } from '@shared/utils/colorGet';
 import styled from 'styled-components';
 
-export const AccordionContainer = styled.div<IAccordion>`
-    
+type IAccordionProps = IAccordion & {
+  isOpen: boolean;
+};
+
+export const AccordionContainer = styled.div<IAccordionProps>`
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+  background: ${({ status }) => colorGet(status, 400)};
+  min-height: 3rem;
+  font-family: sans-serif;
+  width: inherit;
+
+  .accordion-header {
+    padding: 0.7rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+  }
+
+  .accordion-info-text {
+  }
+
+  .accordion-icon {
+    transition: 0.2s ease-in-out;
+    transition-property: transform;
+    transform: ${({ isOpen }) => `rotate(${isOpen ? '-180deg' : '0deg'})`};
+
+    i {
+      display: flex;
+      svg {
+        width: 22px;
+        height: 22px;
+      }
+    }
+  }
+
+  .accordion-content {
+    padding: 0.7rem;
+    background: #ffffff;
+    padding: ${({ isOpen }) => (isOpen ? '1rem 0.7rem' : '0rem 0.7rem')};
+    transition: 0.2s ease-in-out;
+    transition-property: padding;
+  }
 `;
