7d00d4de0394fc19ae493e1b59f81fca6e6ebc34
felipealpino <felipealpino1@gmail.com>
2022-03-19 13:05:41 -0300

    Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable jsx-a11y/no-access-key */
 import { ElementStatus } from '@shared/theme/colors';
-import React, { ButtonHTMLAttributes } from 'react';
-import { ButtonStyleProps } from 'styled-system';
+import React, { HTMLAttributes } from 'react';
 import { ButtonContainer } from './styles';
 
-export interface IButtonProps extends ButtonStyleProps, ButtonHTMLAttributes<HTMLButtonElement> {
+export interface IButton extends HTMLAttributes<HTMLDivElement> {
   status?: ElementStatus;
+  disabled?: boolean;
 }
 
-const Button: React.FC<IButtonProps> = (props) => {
+const Button: React.FC<IButton> = ({ disabled = false, ...props }) => {
   return (
-    <ButtonContainer {...props} className={`button-container ${props.className || ''}`}>
+    <ButtonContainer {...props} disabled={disabled} className={`button-container ${props.className || ''}`}>
       {props.children}
     </ButtonContainer>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,8 +1,9 @@
 import { colorGet } from '@shared/utils/colorGet';
 import styled from 'styled-components';
-import { IButtonProps } from '.';
+import { IButton } from '.';
 
-export const ButtonContainer = styled.button<IButtonProps>`
+export const ButtonContainer = styled.div<IButton>`
+  font-family: sans-serif;
   border: none;
   display: flex;
   align-items: center;
@@ -15,7 +16,7 @@ export const ButtonContainer = styled.button<IButtonProps>`
   transition: 0.1s ease-in-out;
   transition-property: background;
   /* filter: drop-shadow(0px 2px 9px rgba(0, 0, 0, 0.14)); */
-  
+
   background: ${({ status }) => colorGet(status, 500)};
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   border: 2px solid ${({ status }) => colorGet(status, 600)};
