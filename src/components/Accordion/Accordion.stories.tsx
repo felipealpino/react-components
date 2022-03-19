20d40329aa22ce52944604b5fe6603be607861b2
felipealpino <felipealpino1@gmail.com>
2022-03-19 20:00:10 -0300

    exporting components

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+import { withKnobs, select, boolean, text } from '@storybook/addon-knobs';
 import { Accordion } from '../Accordion';
 import { Accordions } from './Accordions';
 
@@ -30,17 +30,39 @@ storiesOf('Accordion', module)
   })
 
   .add('Open One', () => {
+    const gap = text('Gap between Accordions ', '0px');
+
+    const status = select(
+      'Status',
+      ['basic', 'primary', 'secondary', 'tertiary', 'quaternary', 'success', 'info', 'warning', 'danger', 'control'],
+      'control'
+    );
+    
     return (
-      <Accordions gap='20px'>
-        <Accordion title='Accordion 1'>
-          <div>oi</div>
-        </Accordion>
-        <Accordion title='Accordion 2'>
-          <div>oi</div>
-        </Accordion>
-        <Accordion title='Accordion 3'>
-          <div>oi</div>
-        </Accordion>
-      </Accordions>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        <Accordions gap={gap}>
+          <Accordion title='Accordion 1' status='primary'>
+            <div>oi</div>
+          </Accordion>
+          <Accordion title='Accordion 2' disabled status='secondary'>
+            <div>oi</div>
+          </Accordion>
+          <Accordion title='Accordion 3' status='tertiary'>
+            <div>oi</div>
+          </Accordion>
+        </Accordions>
+
+        <Accordions gap={gap}>
+          <Accordion title='Accordion 1' status={status}>
+            <div>oi</div>
+          </Accordion>
+          <Accordion title='Accordion 2' status={status}>
+            <div>oi</div>
+          </Accordion>
+          <Accordion title='Accordion 3' status={status}>
+            <div>oi</div>
+          </Accordion>
+        </Accordions>
+      </div>
     );
   });
